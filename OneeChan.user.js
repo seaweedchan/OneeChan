9eb41138041cbde58b4a9f806ae689dcd4d10cdb
Jordan Bates <saudrapsmann@gmail.com>
2012-09-08 04:14:40 -0700

    Fix board-specific mascots

--- OneeChan.user.js
+++ OneeChan.user.js
@@ -2400,24 +2400,15 @@
                     {
                         j = $SS.conf["Selected Mascots"][i];
 
-                        if ($SS.conf["Mascots"][j] == undefined)
-                        {
-                            $SS.conf["Selected Mascots"].splice(i, 1);
-                            $SS.Config.set("Selected Mascots", $SS.conf["Selected Mascots"]);
-                            continue;
-                        }
-
-                        if ($SS.conf["Mascots"][j].boards != null &&
-                            $SS.conf["Mascots"][j].boards.split(",").indexOf($SS.location.board) == -1)
-                            continue;
-
-                        eMascot.push(j);
+                        if ($SS.conf["Mascots"][j].boards == undefined ||
+                            $SS.conf["Mascots"][j].boards.split(",").indexOf($SS.location.board) !== -1)
+                            eMascot.push(j);
                     }
 
                     if (eMascot.length === 0)
                         return $SS.mascot = new $SS.Mascot(-1);
                     else
-                        mIndex = $SS.conf["Selected Mascots"][Math.floor(Math.random() * eMascot.length)];
+                        mIndex = eMascot[Math.floor(Math.random() * eMascot.length)];
                 }
 
                 $SS.mascot = new $SS.Mascot(mIndex); // Set the active mascot.
