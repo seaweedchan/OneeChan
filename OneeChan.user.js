90ba77b2b8e43a015051a47a0060202b0a3d6245
Jordan Bates <saudrapsmann@gmail.com>
2012-12-10 04:00:54 -0700

    Fix margin between replies

--- OneeChan.user.js
+++ OneeChan.user.js
@@ -172,8 +172,8 @@
                 { name: "Large",      value: 6 },
                 { name: "Normal (4chan default)", value: 4 },
                 { name: "Minimal",  value: 2 },
-                { name: "None",  value: 0 },
-                { name: "Overlapping Borders", value: -2 }
+                { name: "None",  value: -2 },
+                { name: "Overlapping Borders", value: -3 }
             ]
         ],
         "Backlinks Position":
