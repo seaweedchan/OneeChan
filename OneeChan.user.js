d2c680e925164c09033ab3fff10fa50e1e39c921
Jordan Bates <saudrapsmann@gmail.com>
2012-12-13 05:55:02 -0700

    Merge ahodesuka/4chan-Style-Script

--- OneeChan.user.js
+++ OneeChan.user.js
@@ -1694,8 +1694,9 @@
                 // Save Themes
                 $("#themeoptions #tThemes>div").each(function(index)
                 {
-                    if (!$SS.conf["Themes"][index].default && !this.hidden)
-                        themes.push($SS.conf["Themes"][index]);
+                    var oldIndex = parseInt(this.id.substr(5));
+                    if (!$SS.conf["Themes"][oldIndex].default)
+                        themes.push($SS.conf["Themes"][oldIndex]);
                 });
 
                 selectedTheme = (selectedTheme = $("#themeoptions #tThemes>div.selected")).exists() ?
@@ -1712,11 +1713,12 @@
                 // Save Mascots
                 $("#themeoptions #tMascot>div").each(function(index)
                 {
+                    var oldIndex = parseInt(this.id.substr(6));
                     if ($(this).hasClass("selected"))
                         selectedMascots.push(index);
 
-                    if (!$SS.conf["Mascots"][index].default && !this.hidden)
-                        mascots.push($SS.conf["Mascots"][index]);
+                    if (!$SS.conf["Mascots"][oldIndex].default)
+                        mascots.push($SS.conf["Mascots"][oldIndex]);
                 });
 
                 $SS.Config.set("Mascots", mascots);
@@ -1930,10 +1932,9 @@
                 if ($SS.conf["Themes"][tIndex].default &&
                     $SS.conf["Hidden Themes"].push(tIndex) === 1)
                     $("#tThemes a[name=restoreThemes]").show();
-                else
-                    $SS.conf["Themes"].splice(tIndex);
 
-                return $("#theme" + tIndex).remove();
+                return $SS.conf["Themes"][tIndex].default ?
+                    $("#theme" + tIndex).removeClass("selected").hide() : $("#theme" + tIndex).remove();
             },
             showMascot: function(mIndex)
             {
@@ -2055,10 +2056,9 @@
                 if ($SS.conf["Mascots"][mIndex].default &&
                     $SS.conf["Hidden Mascots"].push(mIndex) === 1)
                     $("#tMascot a[name=restoreMascots]").show();
-                else
-                    $SS.conf["Mascots"].splice(mIndex);
 
-                return $("#mascot" + mIndex).remove();
+                return $SS.conf["Mascots"][mIndex].default ?
+                    $("#mascot" + mIndex).removeClass("selected").hide() : $("#mascot" + mIndex).remove();
             },
             SelectImage: function()
             {
