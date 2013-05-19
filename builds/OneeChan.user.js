304fe8cd2c6f4e05f5fc333406cd66f284028a8b
Jordan Bates <saudrapsmann@gmail.com>
2013-05-19 10:25:54 -0700

    Dont care about multiple class support

--- builds/OneeChan.user.js
+++ builds/OneeChan.user.js
@@ -510,17 +510,15 @@
             $(this).removeClass(classNames[j]);
       });
     },
-    optionClass: function(optionName, optionValue, classNames) {
+    optionClass: function(optionName, optionValue, className) {
       return this.each(function()
       {
-        classNames = classNames.split(" ");
-        for (var j = 0, jMAX = classNames.length; j < jMAX; j++)
-          if ($SS.conf[optionName] === optionValue && !$(this).hasClass(classNames[j]))
-            $(this).addClass(classNames[j]);
-          else if ($SS.conf[optionName] !== optionValue && $(this).hasClass(classNames[j]))
-            $(this).removeClass(classNames[j]);
-          else
-            return
+        if ($SS.conf[optionName] === optionValue && !$(this).hasClass(className))
+          $(this).addClass(className);
+        else if ($SS.conf[optionName] !== optionValue && $(this).hasClass(className))
+          $(this).removeClass(className);
+        else
+          return
       });
     },
     remove: function()
