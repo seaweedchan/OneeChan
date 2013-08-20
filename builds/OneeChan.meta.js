695e1fcdd56b64bd6c451ffe73c2e73cc66d7f17
Jordan Bates <saudrapsmann@gmail.com>
2013-08-19 19:18:35 -0700

    Release OneeChan v5.0.22.

--- builds/OneeChan.meta.js
+++ builds/OneeChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OneeChan
-// @version      5.0.21
+// @version      5.0.22
 // @namespace    OneeChan
 // @description  Customizable rice and themes for 4chan and 4chan X.
 // @license      GPLv3; https://github.com/seaweedchan/OneeChan/blob/master/LICENSE 
